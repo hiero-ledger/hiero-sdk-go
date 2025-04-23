380138c04f43ae12d77f58e90fd58e9aa1ccc26b
ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>
2025-04-23 16:13:10 +0300

    refactor: Added helper function for airdrop token transfer params
    Signed-off-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>

--- tck/methods/token.go
+++ tck/methods/token.go
@@ -820,72 +820,9 @@ func (t *TokenService) AirdropToken(_ context.Context, params param.AirdropParam
 	transferParams := *params.TokenTransfers
 
 	for _, transferParam := range transferParams {
-		if transferParam.Token != nil {
-			token := transferParam.Token
-
-			accountId, err := hiero.AccountIDFromString(*token.AccountId)
-			if err != nil {
-				return nil, err
-			}
-
-			tokenId, err := hiero.TokenIDFromString(*token.TokenId)
-			if err != nil {
-				return nil, err
-			}
-
-			amount, err := strconv.ParseInt(*token.Amount, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-
-			if token.Decimals != nil {
-				if transferParam.Approved != nil && *transferParam.Approved {
-					transaction.AddApprovedTokenTransferWithDecimals(tokenId, accountId, amount, uint32(*token.Decimals), true)
-				} else {
-					transaction.AddTokenTransferWithDecimals(tokenId, accountId, amount, uint32(*token.Decimals))
-				}
-			} else {
-				if transferParam.Approved != nil && *transferParam.Approved {
-					transaction.AddApprovedTokenTransfer(tokenId, accountId, amount, true)
-				} else {
-					transaction.AddTokenTransfer(tokenId, accountId, amount)
-				}
-			}
-		} else if transferParam.Nft != nil {
-			nft := transferParam.Nft
-
-			senderAccountId, err := hiero.AccountIDFromString(*nft.SenderAccountId)
-			if err != nil {
-				return nil, err
-			}
-
-			receiverAccountId, err := hiero.AccountIDFromString(*nft.ReceiverAccountId)
-			if err != nil {
-				return nil, err
-			}
-
-			serialNumberParsed, err := strconv.ParseInt(*nft.SerialNumber, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-
-			tokenId, err := hiero.TokenIDFromString(*nft.TokenId)
-			if err != nil {
-				return nil, err
-			}
-
-			nftId := hiero.NftID{
-				TokenID:      tokenId,
-				SerialNumber: serialNumberParsed,
-			}
-
-			if transferParam.Approved != nil && *transferParam.Approved {
-				transaction.AddApprovedNftTransfer(nftId, senderAccountId, receiverAccountId, true)
-			} else {
-				transaction.AddNftTransfer(nftId, senderAccountId, receiverAccountId)
-			}
+		if err := utils.HandleAirdropParam(transaction, transferParam); err != nil {
+			return nil, err
 		}
-
 	}
 
 	if params.CommonTransactionParams != nil {
