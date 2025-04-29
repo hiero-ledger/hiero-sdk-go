7bf49d76895e35717e715f3a883f8ee6807b58f0
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2025-04-29 12:51:01 +0300

    feat(HIP-551): `BatchTransaction` (#1347)
    * feat: initial implementation
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * feat: implement fromBytes
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: fix lint issues
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: revert refactoring
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: refactor
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * test: e2e
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: add retry mechanism
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * feat: add example
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: address feedback
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    ---------
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

---
+++ sdk/batch_transaction.go
@@ -0,0 +1,246 @@
+package hiero
+
+import (
+	"reflect"
+
+	"github.com/hiero-ledger/hiero-sdk-go/v2/proto/services"
+	"github.com/pkg/errors"
+	protobuf "google.golang.org/protobuf/proto"
+)
+
+// SPDX-License-Identifier: Apache-2.0
+
+// BatchTransaction executes multiple transactions in a single consensus event.
+// This allows for atomic execution of multiple transactions, where they either
+// all succeed or all fail together.
+//
+// Requirements:
+// - All inner transactions must be frozen before being added to the batch
+// - All inner transactions must have a batch key set
+// - All inner transactions must be signed as required for each individual transaction
+// - The BatchTransaction must be signed by all batch keys of the inner transactions
+// - Certain transaction types (FreezeTransaction, BatchTransaction) are not allowed in a batch
+//
+// Important notes:
+// - Fees are assessed for each inner transaction separately
+// - The maximum number of inner transactions in a batch is limited to 25
+// - Inner transactions cannot be scheduled transactions
+//
+// Example usage:
+//
+//	batchKey := PrivateKeyGenerateECDSA()
+//
+//	// Create and prepare inner transaction
+//	transaction := NewTransferTransaction().
+//		AddHbarTransfer(sender, amount.Negated()).
+//		AddHbarTransfer(receiver, amount).
+//		Batchify(client, batchKey)
+//
+//	// Create and execute batch transaction
+//	response, err := NewBatchTransaction().
+//		AddInnerTransaction(transaction).
+//		FreezeWith(client).
+//		Sign(batchKey).
+//		Execute(client)
+type BatchTransaction struct {
+	*Transaction[*BatchTransaction]
+	innerTransactions []TransactionInterface
+}
+
+// blacklistedTransactions is a list of transaction types that are not allowed in a batch transaction.
+// These transactions are prohibited due to their special nature or network-level implications.
+var blacklistedTransactions = []reflect.Type{
+	reflect.TypeOf(&FreezeTransaction{}),
+	reflect.TypeOf(&BatchTransaction{}),
+}
+
+// NewBatchTransaction creates a new empty BatchTransaction.
+func NewBatchTransaction() *BatchTransaction {
+	tx := &BatchTransaction{
+		innerTransactions: make([]TransactionInterface, 0),
+	}
+	tx.Transaction = _NewTransaction(tx)
+	return tx
+}
+
+func _BatchTransactionFromProtobuf(tx Transaction[*BatchTransaction], pb *services.TransactionBody) BatchTransaction {
+	var innerTransactions []TransactionInterface
+	for _, innerTransaction := range pb.GetAtomicBatch().Transactions {
+		var transaction services.Transaction
+		transaction.SignedTransactionBytes = innerTransaction
+		transactionBytes, _ := protobuf.Marshal(&transaction)
+		transactionFromBytes, _ := TransactionFromBytes(transactionBytes)
+		innerTransactions = append(innerTransactions, transactionFromBytes)
+	}
+
+	batchTransaction := BatchTransaction{
+		innerTransactions: innerTransactions,
+	}
+	tx.childTransaction = &batchTransaction
+	batchTransaction.Transaction = &tx
+	return batchTransaction
+}
+
+// validateInnerTransaction validates if a transaction is allowed in a batch transaction.
+// A transaction is valid if:
+// - It is not a blacklisted type (FreezeTransaction or BatchTransaction)
+// - It is frozen
+// - It has a batch key set
+func (tx *BatchTransaction) validateInnerTransaction(innerTransaction TransactionInterface) error {
+	// Check for nil transaction
+	if innerTransaction == nil {
+		return errInnerTransactionNil
+	}
+
+	// Validate transaction type is not blacklisted
+	txType := reflect.TypeOf(innerTransaction)
+	for _, blacklistedType := range blacklistedTransactions {
+		if txType == blacklistedType {
+			return errTransactionTypeNotAllowed
+		}
+	}
+
+	innerBaseTransaction := innerTransaction.getBaseTransaction()
+	if !innerBaseTransaction.IsFrozen() {
+		return errInnerTransactionShouldBeFrozen
+	}
+
+	if innerBaseTransaction.GetBatchKey() == nil {
+		return errBatchKeyNotSet
+	}
+
+	return nil
+}
+
+// SetInnerTransactions sets the list of transactions to be executed as part of this BatchTransaction.
+//
+// Requirements for each inner transaction:
+// - Must be frozen (use Transaction.Freeze() or Transaction.FreezeWith(client))
+// - Must have a batch key set (use Transaction.SetBatchKey(key) or Transaction.Batchify(client, key))
+// - Must not be a blacklisted transaction type
+//
+// Returns:
+// - *BatchTransaction: The BatchTransaction instance for fluent API calls
+// - error: If any validation fails
+func (tx *BatchTransaction) SetInnerTransactions(transactions []TransactionInterface) *BatchTransaction {
+	tx._RequireNotFrozen()
+
+	// Validate all transactions before setting
+	for _, transaction := range transactions {
+		if err := tx.validateInnerTransaction(transaction); err != nil {
+			tx.freezeError = err
+			return tx
+		}
+	}
+
+	tx.innerTransactions = make([]TransactionInterface, len(transactions))
+	copy(tx.innerTransactions, transactions)
+	return tx
+}
+
+// AddInnerTransaction appends a transaction to the list of transactions this BatchTransaction will execute.
+//
+// Requirements for the inner transaction:
+// - Must be frozen (use Transaction.Freeze() or Transaction.FreezeWith(client))
+// - Must have a batch key set (use Transaction.SetBatchKey(key) or Transaction.Batchify(client, key))
+// - Must not be a blacklisted transaction type
+//
+// Returns:
+// - *BatchTransaction: The BatchTransaction instance for fluent API calls
+func (tx *BatchTransaction) AddInnerTransaction(innerTransaction TransactionInterface) *BatchTransaction {
+	tx._RequireNotFrozen()
+
+	if err := tx.validateInnerTransaction(innerTransaction); err != nil {
+		tx.freezeError = err
+		return tx
+	}
+
+	tx.innerTransactions = append(tx.innerTransactions, innerTransaction)
+	return tx
+}
+
+// GetInnerTransactions returns the list of transactions this BatchTransaction is currently configured to execute.
+func (tx *BatchTransaction) GetInnerTransactions() []TransactionInterface {
+	return tx.innerTransactions
+}
+
+// GetInnerTransactionIDs returns the list of transaction IDs of each inner transaction of this BatchTransaction.
+//
+// This method is particularly useful after execution to:
+// - Track individual transaction results
+// - Query receipts for specific inner transactions
+// - Monitor the status of each transaction in the batch
+//
+// NOTE: Transaction IDs will only be meaningful after the batch transaction has been
+// executed or the IDs have been explicitly set on the inner transactions.
+func (tx *BatchTransaction) GetInnerTransactionIDs() []TransactionID {
+	var ids []TransactionID
+	for _, tx := range tx.innerTransactions {
+		ids = append(ids, tx.getBaseTransaction().GetTransactionID())
+	}
+	return ids
+}
+
+// ----------- Overridden functions ----------------
+
+func (tx BatchTransaction) getName() string {
+	return "BatchTransaction"
+}
+
+func (tx BatchTransaction) validateNetworkOnIDs(client *Client) error {
+	if client == nil || !client.autoValidateChecksums {
+		return nil
+	}
+
+	for _, innerTx := range tx.innerTransactions {
+		if err := innerTx.validateNetworkOnIDs(client); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (tx BatchTransaction) build() *services.TransactionBody {
+	return &services.TransactionBody{
+		TransactionID:            tx.transactionID._ToProtobuf(),
+		TransactionFee:           tx.transactionFee,
+		TransactionValidDuration: _DurationToProtobuf(tx.GetTransactionValidDuration()),
+		Memo:                     tx.Transaction.memo,
+		Data: &services.TransactionBody_AtomicBatch{
+			AtomicBatch: tx.buildProtoBody(),
+		},
+	}
+}
+
+func (tx BatchTransaction) buildScheduled() (*services.SchedulableTransactionBody, error) {
+	return nil, errors.New("cannot schedule `BatchTransaction`")
+}
+
+func (tx BatchTransaction) buildProtoBody() *services.AtomicBatchTransactionBody {
+	body := &services.AtomicBatchTransactionBody{}
+	for _, innerTransaction := range tx.innerTransactions {
+		request := innerTransaction.makeRequest()
+		transaction, ok := request.(*services.Transaction)
+		if !ok {
+			return nil
+		}
+		body.Transactions = append(body.Transactions, transaction.GetSignedTransactionBytes())
+	}
+
+	return body
+}
+
+func (tx BatchTransaction) getMethod(channel *_Channel) _Method {
+	return _Method{
+		transaction: channel._GetUtil().AtomicBatch,
+	}
+}
+
+func (tx BatchTransaction) constructScheduleProtobuf() (*services.SchedulableTransactionBody, error) {
+	return tx.buildScheduled()
+}
+
+func (tx BatchTransaction) getBaseTransaction() *Transaction[TransactionInterface] {
+	return castFromConcreteToBaseTransaction(tx.Transaction, &tx)
+}
---
+++ sdk/batch_transaction_unit_test.go
@@ -0,0 +1,456 @@
+//go:build all || unit
+// +build all unit
+
+package hiero
+
+// SPDX-License-Identifier: Apache-2.0
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	privateKeyED25519, _ = PrivateKeyFromString("302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10")
+	privateKeyECDSA, _   = PrivateKeyFromStringECDSA("7f109a9e3b0d8ecfba9cc23a3614433ce0fa7ddcc80f2a8f10b222179a5a80d6")
+	validStart           = time.Unix(1554158542, 0)
+)
+
+func spawnTestTransactionAccountCreate() *AccountCreateTransaction {
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	accountID3, _ := AccountIDFromString("0.0.3")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	tx := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID).
+		SetKeyWithoutAlias(privateKeyED25519).
+		SetInitialBalance(NewHbar(450)).
+		SetAccountMemo("some memo").
+		SetReceiverSignatureRequired(true).
+		SetAutoRenewPeriod(10 * time.Hour).
+		SetStakedAccountID(accountID3).
+		SetMaxAutomaticTokenAssociations(100).
+		SetMaxTransactionFee(NewHbar(100000)).
+		SetBatchKey(privateKeyECDSA)
+
+	tx2, err := tx.Freeze()
+	if err != nil {
+		panic(err)
+	}
+	tx2.Sign(privateKeyED25519)
+
+	return tx2
+}
+
+func spawnTestBatchTransaction() *BatchTransaction {
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+	batchKey, err := PrivateKeyGenerateEd25519()
+	if err != nil {
+		panic(err)
+	}
+
+	tx := NewBatchTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	innerTx1 := spawnTestTransactionAccountCreate()
+	innerTx2 := spawnTestTransactionAccountCreate()
+	innerTx3 := spawnTestTransactionAccountCreate()
+
+	tx.AddInnerTransaction(innerTx1)
+	tx.AddInnerTransaction(innerTx2)
+	tx.AddInnerTransaction(innerTx3)
+
+	tx2, err := tx.Freeze()
+	if err != nil {
+		panic(err)
+	}
+	tx2.Sign(batchKey)
+
+	return tx2
+}
+
+func TestUnitBatchTransactionToFromBytes(t *testing.T) {
+	t.Parallel()
+
+	tx := spawnTestBatchTransaction()
+	txBytes, err := tx.ToBytes()
+	require.NoError(t, err)
+
+	txFromBytes, err := TransactionFromBytes(txBytes)
+	require.NoError(t, err)
+
+	assert.Equal(t, tx.buildProtoBody(), txFromBytes.(BatchTransaction).buildProtoBody())
+}
+
+func TestUnitBatchTransactionToFromBytesNoSetters(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	txBytes, err := tx.ToBytes()
+	require.NoError(t, err)
+
+	txFromBytes, err := TransactionFromBytes(txBytes)
+	require.NoError(t, err)
+
+	assert.Equal(t, tx.buildProtoBody(), txFromBytes.(BatchTransaction).buildProtoBody())
+}
+
+func TestUnitBatchTransactionGetInnerTransactions(t *testing.T) {
+	t.Parallel()
+
+	tx := spawnTestBatchTransaction()
+	innerTxs := tx.GetInnerTransactions()
+
+	assert.NotNil(t, innerTxs)
+	assert.Len(t, innerTxs, 3)
+}
+
+func TestUnitBatchTransactionSetInnerTransactions(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	newInnerTxs := []TransactionInterface{
+		spawnTestTransactionAccountCreate(),
+		spawnTestTransactionAccountCreate(),
+	}
+
+	tx2 := tx.SetInnerTransactions(newInnerTxs)
+	assert.Nil(t, tx2.freezeError)
+
+	assert.NotNil(t, tx2.GetInnerTransactions())
+	assert.Len(t, tx2.GetInnerTransactions(), 2)
+	assert.Equal(t, newInnerTxs, tx2.GetInnerTransactions())
+}
+
+func TestUnitBatchTransactionAddInnerTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	newTx := spawnTestTransactionAccountCreate()
+
+	tx.AddInnerTransaction(newTx)
+
+	assert.NotNil(t, tx.GetInnerTransactions())
+	assert.Len(t, tx.GetInnerTransactions(), 1)
+	assert.Contains(t, tx.GetInnerTransactions(), newTx)
+}
+
+func TestUnitBatchTransactionGetInnerTransactionIDs(t *testing.T) {
+	t.Parallel()
+
+	tx := spawnTestBatchTransaction()
+	accountID, _ := AccountIDFromString("0.0.5006")
+	expectedID := TransactionIDGenerate(accountID)
+
+	ids := tx.GetInnerTransactionIDs()
+
+	assert.NotNil(t, ids)
+	assert.Len(t, ids, 3)
+	for _, id := range ids {
+		assert.Equal(t, expectedID.AccountID, id.AccountID)
+	}
+}
+
+func TestUnitBatchTransactionChainedSetters(t *testing.T) {
+	t.Parallel()
+
+	accountID, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	tx := NewBatchTransaction().
+		SetNodeAccountIDs([]AccountID{accountID}).
+		SetTransactionID(transactionID).
+		AddInnerTransaction(spawnTestTransactionAccountCreate())
+
+	tx2, err := tx.Freeze()
+	require.NoError(t, err)
+
+	assert.Len(t, tx2.GetInnerTransactions(), 1)
+	assert.Len(t, tx2.GetNodeAccountIDs(), 1)
+	assert.NotNil(t, tx2.GetTransactionID())
+}
+
+func TestUnitBatchTransactionRejectFreezeTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	freezeTx := NewFreezeTransaction().
+		SetStartTime(time.Now()).
+		SetFreezeType(FreezeTypeFreezeOnly).
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	freezeTx2, err := freezeTx.Freeze()
+	require.NoError(t, err)
+
+	tx2 := tx.AddInnerTransaction(freezeTx2)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errTransactionTypeNotAllowed, tx2.freezeError)
+}
+
+func TestUnitBatchTransactionRejectBatchTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	innerBatchTx := NewBatchTransaction().
+		SetTransactionID(transactionID).
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2})
+
+	innerBatchTx2, _ := innerBatchTx.Freeze()
+
+	tx2 := tx.AddInnerTransaction(innerBatchTx2)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errTransactionTypeNotAllowed, tx2.freezeError)
+}
+
+func TestUnitBatchTransactionRejectBlacklistedTransactionInList(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	validTx := spawnTestTransactionAccountCreate()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	freezeTx := NewFreezeTransaction().
+		SetStartTime(time.Now()).
+		SetFreezeType(FreezeTypeFreezeOnly).
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	freezeTx2, err := freezeTx.Freeze()
+	require.NoError(t, err)
+
+	tx.SetInnerTransactions([]TransactionInterface{validTx, freezeTx2})
+	assert.NotNil(t, tx.freezeError)
+	assert.Equal(t, errTransactionTypeNotAllowed, tx.freezeError)
+}
+
+func TestUnitBatchTransactionRejectNullTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction().AddInnerTransaction(nil)
+	assert.NotNil(t, tx.freezeError)
+	assert.Equal(t, errInnerTransactionNil, tx.freezeError)
+}
+
+func TestUnitBatchTransactionRejectUnfrozenTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	unfrozenTx := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	tx2 := tx.AddInnerTransaction(unfrozenTx)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errInnerTransactionShouldBeFrozen, tx2.freezeError)
+}
+
+func TestUnitBatchTransactionRejectTransactionAfterFreeze(t *testing.T) {
+	t.Parallel()
+
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	tx := NewBatchTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	tx2, _ := tx.Freeze()
+	tx3 := tx2.AddInnerTransaction(spawnTestTransactionAccountCreate())
+
+	assert.NotNil(t, tx3.freezeError)
+	assert.Equal(t, errTransactionIsFrozen, tx3.freezeError)
+}
+
+func TestUnitBatchTransactionRejectTransactionListAfterFreeze(t *testing.T) {
+	t.Parallel()
+
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	tx := NewBatchTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	tx2, _ := tx.Freeze()
+	tx2.SetInnerTransactions([]TransactionInterface{spawnTestTransactionAccountCreate()})
+
+	assert.NotNil(t, tx.freezeError)
+}
+
+func TestUnitBatchTransactionAllowEmptyTransactionList(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	tx2 := tx.SetInnerTransactions([]TransactionInterface{})
+	assert.Nil(t, tx2.freezeError)
+
+	assert.NotNil(t, tx2.GetInnerTransactions())
+	assert.Empty(t, tx2.GetInnerTransactions())
+}
+
+func TestUnitBatchTransactionPreserveTransactionOrder(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	tx1 := spawnTestTransactionAccountCreate()
+	tx2 := spawnTestTransactionAccountCreate()
+	tx3 := spawnTestTransactionAccountCreate()
+
+	transactions := []TransactionInterface{tx1, tx2, tx3}
+	batchTx := tx.SetInnerTransactions(transactions)
+	assert.Nil(t, batchTx.freezeError)
+
+	assert.Equal(t, []TransactionInterface{tx1, tx2, tx3}, batchTx.GetInnerTransactions())
+}
+
+func TestUnitBatchTransactionRejectTransactionWithoutBatchKey(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	txWithoutBatchKey := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	txWithoutBatchKey2, _ := txWithoutBatchKey.Freeze()
+
+	tx2 := tx.AddInnerTransaction(txWithoutBatchKey2)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errBatchKeyNotSet, tx2.freezeError)
+}
+
+func TestUnitBatchTransactionValidateAllTransactionsInList(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	validTx := spawnTestTransactionAccountCreate()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	txWithoutBatchKey := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	txWithoutBatchKey2, _ := txWithoutBatchKey.Freeze()
+
+	tx.SetInnerTransactions([]TransactionInterface{validTx, txWithoutBatchKey2})
+	assert.NotNil(t, tx.freezeError)
+	assert.Equal(t, errBatchKeyNotSet, tx.freezeError)
+}
+
+func TestUnitBatchTransactionValidateMultipleConditions(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	// Test unfrozen transaction with no batch key
+	unfrozenTxWithoutBatchKey := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	tx2 := tx.AddInnerTransaction(unfrozenTxWithoutBatchKey)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errInnerTransactionShouldBeFrozen, tx2.freezeError)
+
+	// Test frozen transaction with no batch key
+	frozenTxWithoutBatchKey, err := unfrozenTxWithoutBatchKey.Freeze()
+	require.NoError(t, err)
+	tx3 := tx.AddInnerTransaction(frozenTxWithoutBatchKey)
+	assert.NotNil(t, tx3.freezeError)
+	assert.Equal(t, errBatchKeyNotSet, tx3.freezeError)
+
+	// Test blacklisted transaction with batch key
+	blacklistedTx := NewFreezeTransaction().
+		SetStartTime(time.Now()).
+		SetFreezeType(FreezeTypeFreezeOnly).
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID).
+		SetBatchKey(privateKeyECDSA)
+	require.NoError(t, err)
+
+	blacklistedTx2, err := blacklistedTx.Freeze()
+	require.NoError(t, err)
+	tx4 := tx.AddInnerTransaction(blacklistedTx2)
+	assert.NotNil(t, tx4.freezeError)
+	assert.Equal(t, errTransactionTypeNotAllowed, tx4.freezeError)
+}
+
+func TestUnitBatchTransactionValidateTransactionStateInOrder(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	transaction := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID)
+
+	// First check should be for frozen state
+	tx2 := tx.AddInnerTransaction(transaction)
+	assert.NotNil(t, tx2.freezeError)
+	assert.Equal(t, errInnerTransactionShouldBeFrozen, tx2.freezeError)
+
+	// After freezing, next check should be for batch key
+	frozenTx, _ := transaction.Freeze()
+	tx3 := tx.AddInnerTransaction(frozenTx)
+	assert.NotNil(t, tx3.freezeError)
+	assert.Equal(t, errBatchKeyNotSet, tx3.freezeError)
+}
+
+func TestUnitBatchTransactionAcceptValidTransaction(t *testing.T) {
+	t.Parallel()
+
+	tx := NewBatchTransaction()
+	accountID1, _ := AccountIDFromString("0.0.5005")
+	accountID2, _ := AccountIDFromString("0.0.5006")
+	transactionID := TransactionIDGenerate(accountID2)
+
+	validTx := NewAccountCreateTransaction().
+		SetNodeAccountIDs([]AccountID{accountID1, accountID2}).
+		SetTransactionID(transactionID).
+		SetBatchKey(privateKeyECDSA)
+
+	validTx2, err := validTx.Freeze()
+	require.NoError(t, err)
+	tx2 := tx.AddInnerTransaction(validTx2)
+
+	assert.Nil(t, tx2.freezeError)
+	assert.NotNil(t, tx2.GetInnerTransactions())
+	assert.Len(t, tx2.GetInnerTransactions(), 1)
+	assert.Contains(t, tx2.GetInnerTransactions(), validTx2)
+}
