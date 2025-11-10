60dddea66e78c0ea784834fd581fd67256b24410
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2025-11-10 12:19:04 +0200

    chore: bump services
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- sdk/node_update_transaction_e2e_test.go
+++ sdk/node_update_transaction_e2e_test.go
@@ -4,15 +4,19 @@ package hiero
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 // SPDX-License-Identifier: Apache-2.0
 
-func TestIntegrationNodeUpdateTransactionCanExecute(t *testing.T) {
-	t.Parallel()
+var (
+	nodeIDToUpdate        = uint64(0)
+	originalNodeAccountId = AccountID{Account: 3}
+)
 
+func TestIntegrationNodeUpdateTransactionCanExecute(t *testing.T) {
 	// Set the network
 	network := make(map[string]AccountID)
 	network["localhost:50211"] = AccountID{Account: 3}
@@ -27,7 +31,7 @@ func TestIntegrationNodeUpdateTransactionCanExecute(t *testing.T) {
 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
 	resp, err := NewNodeUpdateTransaction().
-		SetNodeID(0).
+		SetNodeID(nodeIDToUpdate).
 		SetDescription("testUpdated").
 		SetDeclineReward(true).
 		SetGrpcWebProxyEndpoint(Endpoint{
@@ -42,7 +46,6 @@ func TestIntegrationNodeUpdateTransactionCanExecute(t *testing.T) {
 }
 
 func TestIntegrationNodeUpdateTransactionDeleteGrpcWebProxyEndpoint(t *testing.T) {
-	t.Parallel()
 
 	// Set the network
 	network := make(map[string]AccountID)
@@ -58,7 +61,7 @@ func TestIntegrationNodeUpdateTransactionDeleteGrpcWebProxyEndpoint(t *testing.T
 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
 	resp, err := NewNodeUpdateTransaction().
-		SetNodeID(0).
+		SetNodeID(nodeIDToUpdate).
 		DeleteGrpcWebProxyEndpoint().
 		Execute(client)
 
@@ -68,8 +71,6 @@ func TestIntegrationNodeUpdateTransactionDeleteGrpcWebProxyEndpoint(t *testing.T
 }
 
 func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToTheSameAccount(t *testing.T) {
-	t.Parallel()
-
 	// Set the network
 	network := make(map[string]AccountID)
 	network["localhost:50211"] = AccountID{Account: 3}
@@ -85,9 +86,8 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToTheSameAccount(
 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
 	resp, err := NewNodeUpdateTransaction().
-		SetNodeID(0).
-		SetDescription("testUpdated").
-		SetAccountID(AccountID{Account: 3}).
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(originalNodeAccountId).
 		Execute(client)
 
 	require.NoError(t, err)
@@ -95,35 +95,7 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToTheSameAccount(
 	require.NoError(t, err)
 }
 
-// func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountId(t *testing.T) {
-// 	// Set the network
-// 	network := make(map[string]AccountID)
-// 	network["localhost:50211"] = AccountID{Account: 1038}
-// 	client, err := ClientForNetworkV2(network)
-// 	require.NoError(t, err)
-// 	defer client.Close()
-// 	mirror := []string{"localhost:5600"}
-// 	client.SetMirrorNetwork(mirror)
-
-// 	// Set the operator to be account 0.0.2
-// 	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
-// 	require.NoError(t, err)
-// 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
-
-// 	resp, err := NewNodeUpdateTransaction().
-// 		SetNodeID(0).
-// 		SetDescription("testUpdated").
-// 		SetAccountID(AccountID{Account: 3}).
-// 		Execute(client)
-
-// 	require.NoError(t, err)
-// 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
-// }
-
-func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdInvalidSignature(t *testing.T) {
-	t.Parallel()
-
+func TestIntegrationNodeUpdateTransactionChangeNodeAccountIdMissingAdminSig(t *testing.T) {
 	// Set the network
 	network := make(map[string]AccountID)
 	network["localhost:50211"] = AccountID{Account: 3}
@@ -153,18 +125,51 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdInvalidSignature(
 	client.SetOperator(operator, newOperatorKey)
 
 	resp, err = NewNodeUpdateTransaction().
-		SetNodeID(0).
-		SetDescription("testUpdated").
-		SetAccountID(AccountID{Account: 3}).
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(operator).
 		Execute(client)
 
 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
 	require.ErrorContains(t, err, "exceptional receipt status: INVALID_SIGNATURE")
 }
 
-func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToNonExistentAccountId(t *testing.T) {
-	t.Parallel()
+func TestIntegrationNodeUpdateTransactionChangeNodeAccountIdMissingAccountSig(t *testing.T) {
+	// Set the network
+	network := make(map[string]AccountID)
+	network["localhost:50211"] = AccountID{Account: 3}
+	client, err := ClientForNetworkV2(network)
+	require.NoError(t, err)
+	defer client.Close()
+	mirror := []string{"localhost:5600"}
+	client.SetMirrorNetwork(mirror)
 
+	// Set the operator to be account 0.0.2
+	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
+	require.NoError(t, err)
+	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
+
+	newOperatorKey, err := PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
+	newBalance := NewHbar(2)
+	resp, err := NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newOperatorKey.PublicKey()).
+		SetInitialBalance(newBalance).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+	nodeAccountId := *receipt.AccountID
+
+	resp, err = NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(nodeAccountId).
+		Execute(client)
+
+	_, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.ErrorContains(t, err, "exceptional receipt status: INVALID_SIGNATURE")
+}
+
+func TestIntegrationNodeUpdateTransactionChangeNodeAccountIdToNonExistentAccountId(t *testing.T) {
 	// Set the network
 	network := make(map[string]AccountID)
 	network["localhost:50211"] = AccountID{Account: 3}
@@ -180,20 +185,16 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToNonExistentAcco
 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
 	resp, err := NewNodeUpdateTransaction().
-		SetNodeID(0).
-		SetDescription("testUpdated").
+		SetNodeID(nodeIDToUpdate).
 		SetAccountID(AccountID{Account: 9999999}).
 		Execute(client)
 
 	require.NoError(t, err)
 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-	require.ErrorContains(t, err, "exceptional receipt status: INVALID_NODE_ACCOUNT_ID")
+	require.ErrorContains(t, err, "exceptional receipt status: INVALID_SIGNATURE")
 }
 
 func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToDeletedAccountId(t *testing.T) {
-	t.Parallel()
-	t.Skip("TODO: unskip when services implements check for this")
-
 	// Set the network
 	network := make(map[string]AccountID)
 	network["localhost:50211"] = AccountID{Account: 3}
@@ -231,8 +232,7 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToDeletedAccountI
 	require.NoError(t, err)
 
 	frozen, err := NewNodeUpdateTransaction().
-		SetNodeID(0).
-		SetDescription("testUpdated").
+		SetNodeID(nodeIDToUpdate).
 		SetAccountID(newAccount).
 		FreezeWith(client)
 
@@ -243,124 +243,207 @@ func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountIdToDeletedAccountI
 	require.ErrorContains(t, err, "exceptional receipt status: ACCOUNT_DELETED")
 }
 
-// func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountINoBalance(t *testing.T) {
-// 	t.Parallel()
+func TestIntegrationNodeUpdateTransactionChangeNodeAccountINoBalance(t *testing.T) {
+	// Set the network
+	network := make(map[string]AccountID)
+	network["localhost:50211"] = AccountID{Account: 3}
+	client, err := ClientForNetworkV2(network)
+	require.NoError(t, err)
+	defer client.Close()
+	mirror := []string{"localhost:5600"}
+	client.SetMirrorNetwork(mirror)
 
-// 	// Set the network
-// 	network := make(map[string]AccountID)
-// 	network["localhost:50211"] = AccountID{Account: 3}
-// 	client, err := ClientForNetworkV2(network)
-// 	require.NoError(t, err)
-// 	defer client.Close()
-// 	mirror := []string{"localhost:5600"}
-// 	client.SetMirrorNetwork(mirror)
+	// Set the operator to be account 0.0.2
+	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
+	require.NoError(t, err)
+	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
-// 	// Set the operator to be account 0.0.2
-// 	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
-// 	require.NoError(t, err)
-// 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
+	newAccountKey, err := PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
+	resp, err := NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+	newAccount := *receipt.AccountID
 
-// 	newAccountKey, err := PrivateKeyGenerateEd25519()
-// 	require.NoError(t, err)
-// 	resp, err := NewAccountCreateTransaction().
-// 		SetKeyWithoutAlias(newAccountKey.PublicKey()).
-// 		Execute(client)
-// 	require.NoError(t, err)
-// 	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
-// 	newAccount := *receipt.AccountID
+	_, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
 
-// 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
+	frozen, err := NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(newAccount).
+		FreezeWith(client)
 
-// 	frozen, err := NewNodeUpdateTransaction().
-// 		SetNodeID(0).
-// 		SetDescription("testUpdated").
-// 		SetAccountID(newAccount).
-// 		FreezeWith(client)
+	resp, err = frozen.Sign(newAccountKey).Execute(client)
 
-// 	resp, err = frozen.Sign(newAccountKey).Execute(client)
+	require.NoError(t, err)
+	_, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.ErrorContains(t, err, "exceptional receipt status: NODE_ACCOUNT_HAS_ZERO_BALANCE")
+}
 
-// 	require.NoError(t, err)
-// 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.ErrorContains(t, err, "exceptional receipt status: NODE_ACCOUNT_HAS_ZERO_BALANCE")
-// }
+func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountUpdateAddressbookAndRetry(t *testing.T) {
+	// Set the network
+	network := make(map[string]AccountID)
+	network["localhost:50211"] = originalNodeAccountId
+	network["localhost:51211"] = AccountID{Account: 4}
+	client, err := ClientForNetworkV2(network)
+	require.NoError(t, err)
+	defer client.Close()
+	mirror := []string{"localhost:5600"}
+	client.SetMirrorNetwork(mirror)
 
-// func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountUpdateAddressbookAndRetry(t *testing.T) {
+	// Set the operator to be account 0.0.2
+	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
+	require.NoError(t, err)
+	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
 
-// 	// Set the network
-// 	network := make(map[string]AccountID)
-// 	network["localhost:50211"] = AccountID{Account: 3}
-// 	network["localhost:51211"] = AccountID{Account: 4}
-// 	client, err := ClientForNetworkV2(network)
-// 	require.NoError(t, err)
-// 	defer client.Close()
-// 	mirror := []string{"localhost:5600"}
-// 	client.SetMirrorNetwork(mirror)
+	// create the account that will be the node account id
+	newAccountKey, err := PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
 
-// 	// Set the operator to be account 0.0.2
-// 	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
-// 	require.NoError(t, err)
-// 	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
+	resp, err := NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		SetInitialBalance(NewHbar(1)).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+	newNodeAccountID := *receipt.AccountID
 
-// 	// create the account that will be the node account id
-// 	resp, err := NewAccountCreateTransaction().
-// 		SetKeyWithoutAlias(originalOperatorKey.PublicKey()).
-// 		SetInitialBalance(HbarFrom(1, HbarUnit("hbar"))).
-// 		Execute(client)
-// 	require.NoError(t, err)
-// 	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
-// 	newNodeAccountID := *receipt.AccountID
+	// update node account id
+	frozen, err := NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(newNodeAccountID).
+		FreezeWith(client)
 
-// 	// update node account id
-// 	// 0.0.3 -> 0.0.1003
-// 	resp, err = NewNodeUpdateTransaction().
-// 		SetNodeID(0).
-// 		SetDescription("testUpdated").
-// 		SetAccountID(newNodeAccountID).
-// 		Execute(client)
+	require.NoError(t, err)
+	resp, err = frozen.Sign(newAccountKey).Execute(client)
+	require.NoError(t, err)
 
-// 	require.NoError(t, err)
-// 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
+	// wait for mirror node to import data
+	time.Sleep(time.Second * 5)
 
-// 	// wait for mirror node to import data
-// 	time.Sleep(time.Second * 10)
+	newAccountKey, err = PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
 
-// 	newAccountKey, err := PrivateKeyGenerateEd25519()
-// 	require.NoError(t, err)
-// 	// submit to node 3 and node 4, node 3 fails, node 4 succeeds
-// 	resp, err = NewAccountCreateTransaction().
-// 		SetKeyWithoutAlias(newAccountKey.PublicKey()).
-// 		SetNodeAccountIDs([]AccountID{{Account: 3}, {Account: 4}}).
-// 		Execute(client)
-// 	require.NoError(t, err)
+	// submit to the updated node, retries
+	resp, err = NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		SetNodeAccountIDs([]AccountID{originalNodeAccountId, {Account: 4}}).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
 
-// 	// verify address book has been updated
-// 	key1 := newNodeAccountID
-// 	key2 := AccountID{Account: 4}
-// 	require.Equal(t, newNodeAccountID.String(), client.network.addressBook[key1].AccountID.String())
-// 	require.Equal(t, AccountID{Account: 4}.String(), client.network.addressBook[key2].AccountID.String())
+	// verify address book has been updated
+	key1 := newNodeAccountID
+	key2 := AccountID{Account: 4}
 
-// 	// this transactin should succeed
-// 	resp, err = NewAccountCreateTransaction().
-// 		SetKeyWithoutAlias(newAccountKey.PublicKey()).
-// 		SetNodeAccountIDs([]AccountID{newNodeAccountID}).
-// 		Execute(client)
-// 	require.NoError(t, err)
-// 	receipt, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
+	node1, ok := client.network._GetNodeForAccountID(key1)
+	require.True(t, ok)
+	require.Equal(t, newNodeAccountID.String(), node1.accountID.String())
+	node2, ok := client.network._GetNodeForAccountID(key2)
+	require.True(t, ok)
+	require.Equal(t, AccountID{Account: 4}.String(), node2.accountID.String())
 
-// 	// revert the node account id
-// 	resp, err = NewNodeUpdateTransaction().
-// 		SetNodeID(0).
-// 		SetNodeAccountIDs([]AccountID{newNodeAccountID}).
-// 		SetDescription("testUpdated").
-// 		SetAccountID(AccountID{Account: 3}).
-// 		Execute(client)
+	// this transactin should succeed
+	resp, err = NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		SetNodeAccountIDs([]AccountID{newNodeAccountID}).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
 
-// 	require.NoError(t, err)
-// 	_, err = resp.SetValidateStatus(true).GetReceipt(client)
-// 	require.NoError(t, err)
-// }
+	// revert the node account id
+	resp, err = NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetNodeAccountIDs([]AccountID{newNodeAccountID}).
+		SetAccountID(originalNodeAccountId).
+		Execute(client)
+
+	require.NoError(t, err)
+	_, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+}
+
+func TestIntegrationNodeUpdateTransactionCanChangeNodeAccountWithoutMirrorNodeSetup(t *testing.T) {
+	// Set the network
+	network := make(map[string]AccountID)
+	network["localhost:50211"] = originalNodeAccountId
+	network["localhost:51211"] = AccountID{Account: 4}
+	client, err := ClientForNetworkV2(network)
+	require.NoError(t, err)
+	defer client.Close()
+
+	// Set the operator to be account 0.0.2
+	originalOperatorKey, err := PrivateKeyFromStringEd25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
+	require.NoError(t, err)
+	client.SetOperator(AccountID{Account: 2}, originalOperatorKey)
+
+	// create the account that will be the node account id
+	newAccountKey, err := PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
+
+	resp, err := NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		SetInitialBalance(NewHbar(1)).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err := resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+	newNodeAccountID := *receipt.AccountID
+
+	// update node account id
+	frozen, err := NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(newNodeAccountID).
+		FreezeWith(client)
+
+	require.NoError(t, err)
+	resp, err = frozen.Sign(newAccountKey).Execute(client)
+	require.NoError(t, err)
+
+	newAccountKey, err = PrivateKeyGenerateEd25519()
+	require.NoError(t, err)
+
+	// submit to the updated node, retries
+	resp, err = NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		SetNodeAccountIDs([]AccountID{originalNodeAccountId, {Account: 4}}).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+
+	key1 := originalNodeAccountId
+	key2 := AccountID{Account: 4}
+
+	// verify address book has NOT been updated
+	node1, ok := client.network._GetNodeForAccountID(key1)
+	require.True(t, ok)
+	require.Equal(t, AccountID{Account: 8}.String(), node1.accountID.String())
+	node2, ok := client.network._GetNodeForAccountID(key2)
+	require.True(t, ok)
+	require.Equal(t, AccountID{Account: 4}.String(), node2.accountID.String())
+
+	// this transactin should succeed again because we will retry
+	resp, err = NewAccountCreateTransaction().
+		SetKeyWithoutAlias(newAccountKey.PublicKey()).
+		Execute(client)
+	require.NoError(t, err)
+	receipt, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+
+	// revert the node account id
+	resp, err = NewNodeUpdateTransaction().
+		SetNodeID(nodeIDToUpdate).
+		SetAccountID(originalNodeAccountId).
+		Execute(client)
+
+	require.NoError(t, err)
+	_, err = resp.SetValidateStatus(true).GetReceipt(client)
+	require.NoError(t, err)
+}
