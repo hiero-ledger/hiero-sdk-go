4c8f2ff672bea8fdcb2ee3e093154ac632fbaec9
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2025-11-27 15:20:39 +0200

    HIP-1299: Changing Node Account IDs (#1547)
    * feat: initial impl
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * test: e2e
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * test: e2e
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * test: e2e
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: linter
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * fix: unit
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * fix: test utils
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * fix: e2e
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: revert solo action for e2e tests
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: revert utilities changes
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: fix typo
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * fix: healthy nodes list
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: bump services
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: refactor
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * fix: tests
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * bump mirror
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * bump action
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * get receipt
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: bump solo
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: tidy
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: update deps
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: dual mode
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: bump crypto
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    * chore: bump mirror
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    
    ---------
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- sdk/transaction.go
+++ sdk/transaction.go
@@ -1350,6 +1350,10 @@ func (tx *Transaction[T]) shouldRetry(_ Executable, response interface{}) _Execu
 		return executionStateFinished
 	}
 
+	if status == StatusInvalidNodeAccount {
+		return executionStateRetryWithAnotherNode
+	}
+
 	return executionStateError
 }
 
