bb2a6335375bf1045372cc49cad1fcd379889c0d
ivo <ivo.stoynovski@limechain.tech>
2026-05-08 09:59:24 +0300

    feat(examples): align construct-client with Java SDK
    Two changes to match ConstructClientExample.java:
    - Hardcode HEDERA_NETWORK to "testnet" instead of reading
      from the environment.
    - Set the operator only on testnetClient instead of on all
      four clients.
    
    Other construction patterns (forName, forNetwork, fromConfigFile)
    and the SetLedgerID call on the custom client are preserved.
    
    Signed-off-by: ivo <ivo.stoynovski@limechain.tech>

--- examples/construct_client/main.go
+++ examples/construct_client/main.go
@@ -7,90 +7,103 @@ import (
 	hiero "github.com/hiero-ledger/hiero-sdk-go/v2/sdk"
 )
 
+// HEDERA_NETWORK How to construct and configure a client in different ways.
+//
+// A client has a network and an operator.
+//
+// A Hedera network is made up of nodes — individual servers who participate
+// in the process of reaching consensus on the order and validity of transactions
+// on the network. Three networks you likely know of are previewnet, testnet, and mainnet.
+//
+// For the purpose of connecting to it, each node has an IP address or URL and a port number.
+// Each node also has an AccountID used to refer to that node for several purposes,
+// including the paying of fees to that node when a client submits requests to it.
+//
+// You can configure what network you want a client to use — in other words, you can specify
+// a list of URLs and port numbers with associated account IDs, and
+// when that client is used to execute queries and transactions, the client will
+// submit requests only to nodes in that list.
+//
+// A Client has an operator, which has an AccountID and a PublicKey, and which can
+// sign requests. A client's operator can also be configured.
+const HEDERA_NETWORK = "testnet"
+
 func main() {
-	// Create client for previewnet
-	previewnetClient := hiero.ClientForPreviewnet()
-	// Create client for testnet
+	fmt.Println("Construct Client Example Start!")
+
+	// Here's the simplest way to construct a client.
+	// These clients' networks are filled with default lists of nodes that are baked into the SDK.
+	// Their operators are not yet set, and trying to use them now will result in exceptions.
 	testnetClient := hiero.ClientForTestnet()
-	// Create client for mainnet
+	previewnetClient := hiero.ClientForPreviewnet()
 	mainnetClient := hiero.ClientForMainnet()
 
-	println("Client Construction Example.")
-
-	// Creating client from the set HEDERA_NETWORK environment variable
-	namedNetworkClient, err := hiero.ClientForName(os.Getenv("HEDERA_NETWORK"))
+	// We can also construct a client for testnet, previewnet or mainnet depending on the value of a
+	// network name string. If, for example, the input string equals "testnet", this client will be
+	// configured to connect to testnet.
+	namedNetworkClient, err := hiero.ClientForName(HEDERA_NETWORK)
 	if err != nil {
 		panic(fmt.Sprintf("%v : error creating client for name", err))
 	}
 
-	// Creating account ID of 0.0.3
+	// Let's set the operator on testnetClient.
+	// (The AccountID and PrivateKey here are fake, this is just an example.)
 	id, err := hiero.AccountIDFromString("0.0.3")
 	if err != nil {
 		panic(fmt.Sprintf("%v : error creating AccountID from string", err))
 	}
-
-	// Creating a PrivateKey from a random key string we have
 	key, err := hiero.PrivateKeyFromString("302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10")
 	if err != nil {
 		panic(fmt.Sprintf("%v : error creating PrivateKey from string", err))
 	}
-
-	// Set the operators for each client
 	testnetClient.SetOperator(id, key)
-	mainnetClient.SetOperator(id, key)
-	previewnetClient.SetOperator(id, key)
-	namedNetworkClient.SetOperator(id, key)
 
-	// Create the network map to use
+	// Let's create a client with a custom network.
 	customNetwork := map[string]hiero.AccountID{
 		"2.testnet.hedera.com:50211": {Account: 5},
 		"3.testnet.hedera.com:50211": {Account: 6},
 	}
-
-	// Set network for customClient which uses the above custom network
 	customClient, err := hiero.ClientForNetworkV2(customNetwork)
 	if err != nil {
 		panic(fmt.Sprintf("%v : error creating client for network", err))
 	}
-	// Setting NetworkName for the CustomClient, is only needed if you need to validate ID checksums
+
+	// Since our customClient's network is in this case a subset of testnet, we should set the
+	// ledger ID to testnet. If we don't do this, checksum validation won't work.
 	customClient.SetLedgerID(*hiero.NewLedgerIDTestnet())
 
+	// Let's generate a client from a config.json file.
+	// A config file may specify a network by name, or it may provide a custom network
+	// in the form of a list of nodes.
+	// The config file should specify the operator, so you can use a client constructed
+	// using ClientFromConfigFile() immediately.
 	if os.Getenv("CONFIG_FILE") != "" {
-		// Creating client from a file specified in environment variable CONFIG_FILE
 		configClient, err := hiero.ClientFromConfigFile(os.Getenv("CONFIG_FILE"))
 		if err != nil {
 			panic(fmt.Sprintf("%v : error creating Client from config file", err))
 		}
-
-		// Closing the client from file
 		err = configClient.Close()
 		if err != nil {
 			panic(fmt.Sprintf("%v : error closing configClient", err))
 		}
 	}
 
-	// Clean up, closing each client
-	// Can also do this by using defer in after setting up the client
-	err = previewnetClient.Close()
-	if err != nil {
+	// Always close a client when you're done with it.
+	if err = previewnetClient.Close(); err != nil {
 		panic(fmt.Sprintf("%v : error closing previewnetClient", err))
 	}
-	err = testnetClient.Close()
-	if err != nil {
+	if err = testnetClient.Close(); err != nil {
 		panic(fmt.Sprintf("%v : error closing testnetClient", err))
 	}
-	err = mainnetClient.Close()
-	if err != nil {
+	if err = mainnetClient.Close(); err != nil {
 		panic(fmt.Sprintf("%v : error closing mainnetClient", err))
 	}
-	err = namedNetworkClient.Close()
-	if err != nil {
+	if err = namedNetworkClient.Close(); err != nil {
 		panic(fmt.Sprintf("%v : error closing namedNetworkClient", err))
 	}
-	err = customClient.Close()
-	if err != nil {
+	if err = customClient.Close(); err != nil {
 		panic(fmt.Sprintf("%v : error closing customClient", err))
 	}
 
-	println("Success!")
+	fmt.Println("Construct Client Example Complete!")
 }
